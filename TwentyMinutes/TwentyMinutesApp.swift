3353d598082c307ac0f21227f318ab0996c00698
Thomas Decrick <thomas.decrick@colruytgroup.com>
2023-05-15 15:46:33 +0200

    Initial Commit

---
+++ TwentyMinutes/TwentyMinutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyMinutesApp.swift
+//  TwentyMinutes
+//
+//  Created by Thomas Decrick on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwentyMinutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
