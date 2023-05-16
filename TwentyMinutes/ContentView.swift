061979ca504a645b589c85b94d5ae268776f3a21
Thomas Decrick <thomas.decrick@colruytgroup.com>
2023-05-16 08:37:10 +0200

    year view

--- TwentyMinutes/ContentView.swift
+++ TwentyMinutes/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CalendarMonthView: View {
+    let calendar: Calendar = Calendar.current
+    let month: Date
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        LazyVGrid(columns: createGridColumns(), spacing: 10) {
+            ForEach(0..<numberOfDaysInMonth(), id: \.self) { day in
+                Text("\(day + 1)")
+            }
         }
         .padding()
+    }
+
+    private func createGridColumns() -> [GridItem] {
+        Array(repeating: GridItem(.flexible()), count: 7)
+    }
+
+    private func numberOfDaysInMonth() -> Int {
+        let range = calendar.range(of: .day, in: .month, for: month)!
+        return range.count
+    }
+}
+
+
+struct CalendarYearView: View {
+    let calendar: Calendar = Calendar.current
+    let startDate: Date
+    let yearCount: Int
+
+    var body: some View {
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 20) {
+                ForEach(0..<yearCount) { index in
+                    CalendarMonthView(month: calendar.date(byAdding: .month, value: index, to: startDate)!)
+                        .id(index)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        CalendarYearView(startDate: Date(), yearCount: 5)
     }
 }
 
